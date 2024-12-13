f9cfdb07b7c8df397030f41e97622eb69d608339
S42yt <songoku42@outlook.de>
2024-12-13 13:49:56 +0300

    changed ui

--- src/App.tsx
+++ src/App.tsx
@@ -119,40 +119,47 @@ function App() {
   }
 
   return (
-    <main className="container">
-      <h1>Video Downloader</h1>
-      <p>Download Videos or MP3's from many Platforms</p>
+    <div className="app-container">
+      <header className="header">
+        <h1>Desto Ripper</h1>
+      </header>
+      <main className="container">
+        <p>Download Videos or MP3's from many Platforms</p>
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (validateUrl()) {
-            convert();
-          }
-        }}
-      >
-        <div className="input-container">
-          <input
-            id="url-input"
-            value={displayedUrl}
-            onChange={handleInputChange}
-            placeholder={"Paste the Video URL..."}
-            style={error ? { borderColor: "red", color: "red" } : {}}
-          />
-        </div>
-        <button type="submit">Convert</button>
-      </form>
-      <Toaster />
-      <form>
-        <h2 className="PlatformSupportH2">Platforms we Support:</h2>
-        <div className="platformsIMG">
-          <svg className="ytIMG" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z" /> </svg>
-          <img className="ttIMG" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw2AHEa003m1a2SgHiCuL3NHMzcLkkQsUEVA&s" alt="TikTok"/>
-          <img className="igIMG" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK5zQ42vVBoRGGYD7A6pKnIIRAT_G-cFVZbA&s" alt="Instagram" />
-        </div>
-      </form>
-    </main>
+        <form
+          className="row"
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (validateUrl()) {
+              convert();
+            }
+          }}
+        >
+          <div className="input-container">
+            <input
+              id="url-input"
+              value={displayedUrl}
+              onChange={handleInputChange}
+              placeholder={"Paste the Video URL..."}
+              style={error ? { borderColor: "red", color: "red" } : {}}
+            />
+          </div>
+          <button type="submit">Convert</button>
+        </form>
+        <Toaster />
+        <form>
+          <h2 className="PlatformSupportH2">Platforms we Support:</h2>
+          <div className="platformsIMG">
+            <svg className="ytIMG" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z" /> </svg>
+            <img className="ttIMG" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw2AHEa003m1a2SgHiCuL3NHMzcLkkQsUEVA&s" alt="TikTok"/>
+            <img className="igIMG" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK5zQ42vVBoRGGYD7A6pKnIIRAT_G-cFVZbA&s" alt="Instagram" />
+          </div>
+        </form>
+      </main>
+      <footer className="footer">
+        <p>&copy; 2025 Desto.lol. All rights reserved.</p>
+      </footer>
+    </div>
   );
 }
 
