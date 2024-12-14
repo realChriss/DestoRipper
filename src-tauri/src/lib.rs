5f7d811d715dd6ffc16775b094bd3a393e255b2b
Chriss <77172965+realChriss@users.noreply.github.com>
2024-12-14 21:34:28 +0100

    refactor and events

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,11 +5,12 @@ use tokio::process::Command;
 use tokio::io::AsyncReadExt;
 use std::{env, fs};
 use std::time::{SystemTime, UNIX_EPOCH};
+use tauri::{Emitter, Window};
 
 #[tauri::command]
-async fn submit(url: String, format: String) -> bool {
+async fn submit(url: String, format: String, window: tauri::Window) -> bool {
     // Versuche die Hauptlogik auszuführen
-    if let Err(err) = submit_impl(url, format).await {
+    if let Err(err) = submit_impl(url, format, window).await {
         eprintln!("Fehler beim Herunterladen: {}", err);
         false
     } else {
@@ -17,9 +18,12 @@ async fn submit(url: String, format: String) -> bool {
     }
 }
 
-async fn submit_impl(url: String, format: String) -> Result<(), Box<dyn std::error::Error>> {
+async fn submit_impl(
+    url: String,
+    format: String,
+    window: Window, // Fenster für Events
+) -> Result<(), Box<dyn std::error::Error>> {
     let unix = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis();
-    
     let ytdlp_path = get_ytdlp_path();
     let ffmpeg_path = get_ffmpeg_path();
 
@@ -29,11 +33,11 @@ async fn submit_impl(url: String, format: String) -> Result<(), Box<dyn std::err
     let video_path = download_dir.join(unix.to_string() + "video.tmp");
     let audio_path = download_dir.join(unix.to_string() + "audio.tmp");
 
-    // Separate Streams für Video und Audio
     let mut video_stream = Vec::new();
     let mut audio_stream = Vec::new();
 
-    println!("Start download..");
+    // Fortschritt "Download gestartet"
+    window.emit("progress", "downloading")?;
 
     // Video-Stream extrahieren
     let mut video_child = Command::new("python")
@@ -41,16 +45,14 @@ async fn submit_impl(url: String, format: String) -> Result<(), Box<dyn std::err
         .arg("--ffmpeg-location")
         .arg(&ffmpeg_path)
         .arg("-f")
-        .arg("bestvideo") //[vcodec^=avc1]
+        .arg("bestvideo")
         .arg("--no-part")
         .arg("-o")
         .arg("-")
         .arg(&url)
         .stdout(Stdio::piped())
-        .creation_flags(0x08000000) // No Window
         .spawn()?;
 
-    // Audio-Stream extrahieren
     let mut audio_child = Command::new("python")
         .arg(&ytdlp_path)
         .arg("--ffmpeg-location")
@@ -62,10 +64,9 @@ async fn submit_impl(url: String, format: String) -> Result<(), Box<dyn std::err
         .arg("-")
         .arg(&url)
         .stdout(Stdio::piped())
-        .creation_flags(0x08000000)
         .spawn()?;
 
-    // Video-Stream in Speicher laden
+    
     if let Some(mut video_stdout) = video_child.stdout.take() {
         let mut buffer = [0u8; 8192];
         loop {
@@ -76,8 +77,7 @@ async fn submit_impl(url: String, format: String) -> Result<(), Box<dyn std::err
             video_stream.extend_from_slice(&buffer[..bytes_read]);
         }
     }
-
-    // Audio-Stream in Speicher laden
+    
     if let Some(mut audio_stdout) = audio_child.stdout.take() {
         let mut buffer = [0u8; 8192];
         loop {
@@ -89,23 +89,25 @@ async fn submit_impl(url: String, format: String) -> Result<(), Box<dyn std::err
         }
     }
 
-    // Warten auf Beendigung der Child-Prozesse
+    window.emit("progress", "processing")?;
+
     video_child.wait().await?;
     audio_child.wait().await?;
 
     println!("Video-Stream: {} Bytes", video_stream.len());
     println!("Audio-Stream: {} Bytes", audio_stream.len());
 
-    fs::write(&video_path, &video_stream)?;
     fs::write(&audio_path, &audio_stream)?;
+    fs::write(&video_path, &video_stream)?;
 
     println!("Running FFmpeg..");
-    // FFmpeg-Prozess zum Zusammenführen
+
+    window.emit("progress", "Merging video and audio...")?;
     let mut ffmpeg_child = Command::new(&ffmpeg_path)
         .arg("-i")
-        .arg(&video_path)  // Video-Input
+        .arg(&video_path)
         .arg("-i")
-        .arg(&audio_path)  // Audio-Input
+        .arg(&audio_path)
         .arg("-c:v")
         .arg("libx264")
         .arg("-c:a")
@@ -114,20 +116,17 @@ async fn submit_impl(url: String, format: String) -> Result<(), Box<dyn std::err
         .arg("0:v")
         .arg("-map")
         .arg("1:a")
+        .arg("-threads")
+        .arg("0")
         .arg(download_path.to_str().unwrap())
         .arg("-y")
-        .stdin(Stdio::piped())
-        .creation_flags(0x08000000)
         .spawn()?;
-    
-    // FFmpeg-Prozess beenden
+
     ffmpeg_child.wait().await?;
 
     fs::remove_file(&video_path)?;
     fs::remove_file(&audio_path)?;
-
-    println!("Done");
-
+    
     Ok(())
 }
 
--- src/App.tsx
+++ src/App.tsx
@@ -3,165 +3,204 @@ import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 import React from "react";
 import { Toaster, toast } from "react-hot-toast";
+import { listen } from "@tauri-apps/api/event";
 
 function App() {
-  const [url, setUrl] = useState("");
-  const [error, setError] = useState("");
-  const [displayedUrl, setDisplayedUrl] = useState("");
-  const typingTimeoutRef = useRef<number | undefined>();
-  const isTypingRef = useRef(false);
+    const [url, setUrl] = useState("");
+    const [error, setError] = useState("");
+    const [displayedUrl, setDisplayedUrl] = useState("");
+    const typingTimeoutRef = useRef<number | undefined>();
+    const isTypingRef = useRef(false);
 
-  useEffect(() => {
-    let intervalId: number | undefined;
+    useEffect(() => {
+        let intervalId: number | undefined;
 
-    if (url.length > 15 && !isTypingRef.current) {
-      intervalId = window.setInterval(() => {
-        setDisplayedUrl((prev) => {
-          const spacedUrl = ` ${prev} `;
-          return spacedUrl.substring(1) + spacedUrl[0];
-        });
-      }, 50);
-    } else {
-      setDisplayedUrl(url);
-    }
+        if (url.length > 15 && !isTypingRef.current) {
+            intervalId = window.setInterval(() => {
+                setDisplayedUrl((prev) => {
+                    const spacedUrl = ` ${prev} `;
+                    return spacedUrl.substring(1) + spacedUrl[0];
+                });
+            }, 50);
+        } else {
+            setDisplayedUrl(url);
+        }
 
-    return () => {
-      if (intervalId !== undefined) {
-        clearInterval(intervalId);
-      }
-    };
-  }, [url, isTypingRef.current]);
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    let newValue = e.currentTarget.value;
-
-    if (newValue === "biogg.net") {
-      window.location.href = "https://biogg.net";
-      setUrl(newValue);
-      setDisplayedUrl(newValue);
-      return;
-    }
-
-    setUrl(newValue);
-    setDisplayedUrl(newValue);
-
-    if (typingTimeoutRef.current) {
-      clearTimeout(typingTimeoutRef.current);
-    }
-
-    isTypingRef.current = true;
-
-    typingTimeoutRef.current = window.setTimeout(() => {
-      isTypingRef.current = false;
-    }, 3000);
-  };
-
-  async function convert() {
-    const loadingToastId = toast.loading("Download is starting...", {
-      style: {
-        borderRadius: "8px",
-        background: "#333",
-        color: "#fff",
-      },
-    });
-
-    try {
-      const result = await invoke("submit", { url, format: "mp4" });
-
-      if (!result) {
-        throw new Error("Failed to reach the platform");
-      }
-
-      toast.dismiss(loadingToastId);
-      toast.success("Download completed!", {
-        style: {
-          borderRadius: "8px",
-          background: "#333",
-          color: "#fff",
-        },
-      });
-    } catch (error) {
-      setTimeout(() => {
-        toast.dismiss(loadingToastId);
-        toast.error("An error occurred or platform could not be reached.", {
-          style: {
-            borderRadius: "8px",
-            background: "#333",
-            color: "#fff",
-          },
-        });
-      }, 2000);
-    }
-  }
-
-  function validateUrl() {
-    return true
-    const youtubeRegex = /^https:\/\/www\.youtube\.com\/watch\?v=[\w-]{11}$/;
-    const tiktokRegex = /^https:\/\/(www\.)?tiktok\.com\/[\@\w.-]+\/video\/\d+$/;
-    const instagramRegex = /^https:\/\/(www\.)?instagram\.com\/p\/[\w-]+\/$/;
-    const redditRegex = /^https:\/\/(www\.)?reddit\.com\/r\/[\w-]+\/comments\/[\w-]+\//;
-    const xRegex = /^https:\/\/(www\.)?x\.com\/[\@\w.-]+\/status\/\d+$/;
-
-    if (youtubeRegex.test(url)) return true;
-    if (tiktokRegex.test(url)) return true;
-    if (instagramRegex.test(url)) return true;
-    if (redditRegex.test(url)) return true;
-    if (xRegex.test(url)) return true;
-
-    setError("This is not a valid video link for a supported platform.");
-    toast.error("This is not a valid video link for a supported platform.", {
-      style: {
-        borderRadius: "8px",
-        background: "#333",
-        color: "#fff",
-      },
-    });
-    return false;
-  }
-
-  return (
-    <div className="app-container">
-      <header className="header">
-        <h1>Desto Ripper</h1>
-      </header>
-      <main className="container">
-        <p>Download Videos or MP3's from many Platforms</p>
-
-        <form
-          className="row"
-          onSubmit={(e) => {
-            e.preventDefault();
-            if (validateUrl()) {
-              convert();
+        return () => {
+            if (intervalId !== undefined) {
+                clearInterval(intervalId);
             }
-          }}
-        >
-          <div className="input-container">
-            <input
-              id="url-input"
-              value={displayedUrl}
-              onChange={handleInputChange}
-              placeholder={"Paste the Video URL..."}
-              style={error ? { borderColor: "red", color: "red" } : {}}
-            />
-          </div>
-          <button type="submit">Convert</button>
-        </form>
-        <Toaster />
-        <form>
-          <h2 className="PlatformSupportH2">Platforms we Support:</h2>
-          <div className="platformsIMG">
-            <svg className="ytIMG" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z" /> </svg>
-            <img className="ttIMG" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw2AHEa003m1a2SgHiCuL3NHMzcLkkQsUEVA&s" alt="TikTok"/>
-            <img className="igIMG" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK5zQ42vVBoRGGYD7A6pKnIIRAT_G-cFVZbA&s" alt="Instagram" />
-          </div>
-        </form>
-      </main>
-      <footer className="footer">
-        <p>&copy; 2025 Desto.lol. All rights reserved.</p>
-      </footer>
-    </div>
-  );
+        };
+    }, [url, isTypingRef.current]);
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        let newValue = e.currentTarget.value;
+
+        if (newValue === "biogg.net") {
+            window.location.href = "https://biogg.net";
+            setUrl(newValue);
+            setDisplayedUrl(newValue);
+            return;
+        }
+
+        setUrl(newValue);
+        setDisplayedUrl(newValue);
+
+        if (typingTimeoutRef.current) {
+            clearTimeout(typingTimeoutRef.current);
+        }
+
+        isTypingRef.current = true;
+
+        typingTimeoutRef.current = window.setTimeout(() => {
+            isTypingRef.current = false;
+        }, 3000);
+    };
+
+    async function convert() {
+        const loadingToastId = toast.loading("Starting...", {
+            style: {
+                borderRadius: "8px",
+                background: "#333",
+                color: "#fff",
+            },
+        });
+
+        const update = (message: string) => {
+            toast.loading(message, {
+                id: loadingToastId, // Bestehendes Toast aktualisieren
+                style: {
+                    borderRadius: "8px",
+                    background: "#333",
+                    color: "#fff",
+                },
+            });
+        }
+        
+        listen("progress", (event) => {
+
+            const message = event.payload;
+            switch (message) {
+                case "downloading":
+                    update("Downloading...")
+                    break
+                case "processing":
+                    update("Processing...")
+                    break
+            }
+        })
+    
+        try {
+            const result = await invoke("submit", { url, format: "mp4" });
+
+            if (!result) {
+                throw new Error("Failed to reach the platform");
+            }
+
+            toast.dismiss(loadingToastId);
+            toast.success("Download completed!", {
+                style: {
+                    borderRadius: "8px",
+                    background: "#333",
+                    color: "#fff",
+                },
+            });
+        } catch (error) {
+            toast.dismiss(loadingToastId);
+            toast.error("An error occurred or platform could not be reached.", {
+                style: {
+                    borderRadius: "8px",
+                    background: "#333",
+                    color: "#fff",
+                },
+            });
+        }
+    }
+
+
+
+    function validateUrl() {
+        return true
+        const youtubeRegex = /^https:\/\/www\.youtube\.com\/watch\?v=[\w-]{11}$/;
+        const tiktokRegex = /^https:\/\/(www\.)?tiktok\.com\/[\@\w.-]+\/video\/\d+$/;
+        const instagramRegex = /^https:\/\/(www\.)?instagram\.com\/p\/[\w-]+\/$/;
+        const redditRegex = /^https:\/\/(www\.)?reddit\.com\/r\/[\w-]+\/comments\/[\w-]+\//;
+        const xRegex = /^https:\/\/(www\.)?x\.com\/[\@\w.-]+\/status\/\d+$/;
+
+        if (youtubeRegex.test(url)) return true;
+        if (tiktokRegex.test(url)) return true;
+        if (instagramRegex.test(url)) return true;
+        if (redditRegex.test(url)) return true;
+        if (xRegex.test(url)) return true;
+
+        setError("This is not a valid video link for a supported platform.");
+            toast.error("This is not a valid video link for a supported platform.", {
+                style: {
+                    borderRadius: "8px",
+                    background: "#333",
+                    color: "#fff",
+                },
+            });
+        return false;
+    }
+
+    return (
+        <div className="app-container">
+            <header className="header">
+                <h1>Desto Ripper</h1>
+            </header>
+            <main className="container">
+                <p>Download Videos or MP3's from many Platforms</p>
+
+                <form
+                    className="row"
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        if (validateUrl()) {
+                            convert();
+                        }
+                    }}
+                >
+                    <div className="input-container">
+                        <input
+                            id="url-input"
+                            value={displayedUrl}
+                            onChange={handleInputChange}
+                            placeholder={"Paste the Video URL..."}
+                            style={error ? { borderColor: "red", color: "red" } : {}}
+                        />
+                    </div>
+                    <button type="submit">Convert</button>
+                </form>
+                <Toaster />
+                <form>
+                    <h2 className="PlatformSupportH2">Platforms we Support:</h2>
+                    <div className="platformsIMG">
+                        <svg
+                            className="ytIMG"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 576 512"
+                        >
+                            <path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z" />
+                        </svg>
+                        <img
+                            className="ttIMG"
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw2AHEa003m1a2SgHiCuL3NHMzcLkkQsUEVA&s"
+                            alt="TikTok"
+                        />
+                        <img
+                            className="igIMG"
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK5zQ42vVBoRGGYD7A6pKnIIRAT_G-cFVZbA&s"
+                            alt="Instagram"
+                        />
+                    </div>
+                </form>
+            </main>
+            <footer className="footer">
+                <p>&copy; 2025 Desto.lol. All rights reserved.</p>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
